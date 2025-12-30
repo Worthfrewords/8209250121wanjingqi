5ea9bd2159251e095b160aaab6824fe1ba6133fa
Worthfrewords <3471008339@qq.com>
2025-12-30 20:10:38 +0800

    a

---
+++ 3.3.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+//using namespace std;
+//#include"mytemperature.h"
+//int main()
+//{
+//	double cel, fah;
+//	cout << "输入一个摄氏温度:";
+//	cin >> cel;
+//	cout << "转化为华氏温度为：" << celsius_to_fah(cel) << endl;
+//	cout << "输入一个华氏温度:";
+//	cin >> fah;
+//	cout << "转化为摄氏温度为：" << fahrenheit_to_cels(fah) << endl;
+//}
