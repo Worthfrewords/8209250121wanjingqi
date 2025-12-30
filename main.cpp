d58df65830a184d2fcee4fedf9739301a8897d55
Worthfrewords <3471008339@qq.com>
2025-12-30 20:18:18 +0800

    a

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+//#include"student.h"
+//using namespace std;
+//int main()
+//{
+//	Student stud;
+//	Student stud1(007, "tcg", 'm');
+//	stud.display();
+//	stud1.display();
+//	return 0;
+//}
