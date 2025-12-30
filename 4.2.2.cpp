e1e94b134bd94e6f974127f55a762f0fa799df14
Worthfrewords <3471008339@qq.com>
2025-12-30 20:16:07 +0800

    a

---
+++ 4.2.2.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//using namespace std;
+//int* f()
+//{
+//	int* list = new int[4];
+//	for(int i=0;i<4;i++){
+//		list[i] = i+1;
+//	}
+//	return list;
+//}
+//void main()
+//{
+//	int* p = f();
+//	cout << p[0] << endl;
+//	cout << p[1] << endl;
+//	delete[] p;
+//}
