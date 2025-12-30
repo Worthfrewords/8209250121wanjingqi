9c1a642ab5fcd615ecec7dcb6fe24ab42cc9c4af
Worthfrewords <3471008339@qq.com>
2025-12-30 20:17:06 +0800

    a

---
+++ 5.2.cpp
@@ -0,0 +1,38 @@
+//#include<iostream>
+//using namespace std;
+//class cube
+//{
+//public:
+//	double length, width, height;
+//	cube()
+//	{
+//		length = 0;
+//		width = 0;
+//		height = 0;
+//	}
+//	void scan_c()
+//	{
+//		double len, wid, hei;
+//		cout << "请输入长方体的长宽高:" << endl;
+//		cin >> len >> wid >> hei;
+//		length = len;
+//		width = wid;
+//		height = hei;
+//	}
+//	void v()
+//	{
+//		double v = 0;
+//		v = length * width * height;
+//		cout << "长方体的体积为： " << v << endl;
+//	}
+//};
+//int main()
+//{
+//	cube c;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		c.scan_c();
+//		c.v();
+//	}
+//	return 0;
+//}
