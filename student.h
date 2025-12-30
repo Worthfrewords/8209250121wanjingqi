ae513c4b6c956cd06661b985ef5a8fc5747fb0df
Worthfrewords <3471008339@qq.com>
2025-12-30 20:20:06 +0800

    a

---
+++ student.h
@@ -0,0 +1,15 @@
+//#pragma once
+//#include<iostream>
+//using namespace std;
+//class Student
+//{
+//public:
+//	void display();
+//	void set_value();
+//	Student(int Num, const char Name[20], char Sex);
+//	Student();
+//private:
+//	int num;
+//	char name[20];
+//	char sex;
+//};
